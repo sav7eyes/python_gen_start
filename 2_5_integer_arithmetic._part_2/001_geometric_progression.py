fb880a50789ef560035796125a95dc59907fee0c
Andrey Semiglazov (sav7eyes) <sav7eyes@gmail.com>
2024-08-23 20:17:18 +0300

    Add 2.4 && 2.5 Integer arithmetic

---
+++ 2_5_integer_arithmetic._part_2/001_geometric_progression.py
@@ -0,0 +1,4 @@
+first_term_progression = int(input())
+denominator_progression = int(input())
+n_term_progression = int(input())
+print(first_term_progression * denominator_progression**(n_term_progression - 1))
